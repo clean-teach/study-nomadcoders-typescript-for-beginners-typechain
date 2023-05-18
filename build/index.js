1d9806397a1315d6400909bb34bf13c7a84906e0
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-05-19 01:31:04 +0900

    #5.3 Declaration File 및 .prettierrc 추가
    ### 목표
    
    - 배경 :
        - 타입스크립트는 자바스크립트가 제공하는 기본 기능들에 대한 자동완성 기능을 제공한다.
    - 이번 강의에서는 타입스크립트가 어떻게 그러한 정보를 아는지에 대해서 배워본다.
    - 직접 ****Declaration File**** 을 작성해서 타입 정의를 해보고 자동완성 기능을 구현해 본다.
        - node_modules 에 설치된 자바스크립트 모듈을 타입스크립트에서 어떻게 사용하는지 가상의 파일을 만들어서 실습해본다.
            - `npm` 으로부터 자바스크립트 패키지를 다운 받을 경우..
    
    ### Type Definitions
    
    - 타입 정의
    
    > *“타입스크립트는 내장된 자바스크립트 API를 위한 기본적인 타입 정의는 가지고 있다. (예 : `Math`)”*
    >
    - 사람들이 미리 `document`, `localStorage`, `window`, `Math` 등의 구조(argument, return, type 등)의 설명서를 정리 해 놓았다.
    - 타입스크립트가 몇몇 자바스크립트 코드와 API 타입을 설명 할 수 있도록 해준다.
    - **Type Definitions 을 작성해야 하는 이유** :
        - 많은 라이프러리, 프레임워크, 패키지들은 타입스크립트가 아니라, 자바스크립트로 만들어져 있다.
        - 타입스크립트 환경의 프로젝트에서 자바스크립트로 만들어진 라이브러리, 프레임워크, 패키지들을 사용 할 때, 이것들의 타입을 알 수 있는 방법이 없다.
            - 기본적으로 타입스크립트 환경에서도 자바스크립트 코드가 동작하기는 하지만,
            - 그 코드들의 타입을 알기 위해서는 **타입 정의(**Type Definitions**)** 가 필요하다.
    
    ### `strict`
    
    [TSConfig Reference - Docs on every TSConfig option](https://www.typescriptlang.org/tsconfig#strict)
    
    ```json
    {
    	...
    	"compilerOptions" : {
    		"strict": true
    	}
    }
    ```
    
    - 모든 엄격한 타입 검사 옵션을 활성화
    - `strict` 플래그는 프로그램 정확성을 더 강력하게 보장하는 광범위한 타입 검사 동작을 가능하게 한다.
    
    ### d.ts
    
    - ****Declaration File 의 확장자****
    - 자동완성 기능은 마법이 아니다.
        - 누군가가 타입 정의 파일을 작성했기 때문에 가능 한 것이다.
        - 누군가 작성한 d.ts 파일을 타입스크립트가 읽어주는 것 뿐이다.
    - 호출 시그니처 뿐만 아니라, 주석을 통해서 간단한 설명도 작성 할 수 있다.
    
    ### 직접 d.ts 파일을 작성해보기
    
    1. 가상의 자바스크립트 패키지 파일을 생성
        - `fileName.js`
    
            ```jsx
            export function init(config) {
              return true;
            }
            export function exit(code) {
              return code + 1;
            }
            ```
    
    2. 자바스크립트 파일과 동일한 이름의 d.ts 파일 작성
        - `fileName.d.ts`
    
            ```tsx
            interface Config {
              url: string;
            }
            declare module 'myPackage' {
              function init(config: Config): boolean;
              function exit(code: number): number;
            }
            ```
    
            - d.ts 파일에서는 직접 구현하는 것이 아니라, 구현된 자바스크립트 파일의 타입만 호출 시그니처 형태로 정리한다.

--- build/index.js
+++ build/index.js
@@ -1,10 +1,6 @@
-var hello = function () { return 'hi'; };
-var Block = /** @class */ (function () {
-    function Block(data) {
-        this.data = data;
-    }
-    Block.hello = function () {
-        return 'hi';
-    };
-    return Block;
-}());
+import { init, exit } from 'myPackage';
+init({
+    url: 'true',
+});
+exit(0);
+localStorage.clear();
